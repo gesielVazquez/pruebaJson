8f3acc2fe4a5d975c58c63e14633f5be65a5fd3e
Gesiel V <the.alucard.legna@gmail.com>
2019-05-07 18:34:54 -0500

    Transforacion del Json en un array's, impresion de cada array, comparacion de dato en nivel y calculo de porcentaje de bono grupal.

--- json/procesar.php
+++ json/procesar.php
@@ -1,16 +1,44 @@
 <?php
 
 $json_file = file_get_contents('prueba.json');
-print_r($json_file);
+
+$json_to_array = json_decode($json_file, True);
+
+$total_goles_equipo = 0;
+$total_goles_nivel = 0;
+
+$niveles = array('A' => 5, 'B' => 10, 'C' => 15, 'Cuauh' => 20);
+  foreach ($json_to_array as $jugador) {
+    echo "</br></br> Jugador: ";
+    print_r($jugador);
+      if (!empty($jugador['sueldo'])){
+        $total_goles_equipo += $jugador['goles'];
+        $total_goles_nivel += $niveles[$jugador['nivel']];
+      }
+  }
+
+  echo ("</br></br> Los Goles totales del equipo son : $total_goles_equipo");
+  echo ("</br></br> El nivel goles necesario por equipo es de : $total_goles_nivel goles en total");
+
+$porcen_bono_grupal = 0;
+
+  if ($total_goles_equipo >= $total_goles_nivel){
+    $porcen_bono_grupal = 100;
+  }
+  else {
+    $porcen_bono_grupal = ($total_goles_equipo / $total_goles_nivel)*100;
+  }
+
+  echo ("</br></br> El porcentaje de bono por equipo es : $porcen_bono_grupal");
 
 $file = "prueba.json";
 if (!unlink($file))
   {
-  echo ("Error No se pudo borrar el archivo $file");
+  echo ("</br></br> Error No se pudo borrar el archivo $file");
   }
 else
   {
-  echo ("Archivo $file borrado satisfactoriamente.");
+  echo ("</br></br> Archivo $file borrado satisfactoriamente.");
   }
 
  ?>
